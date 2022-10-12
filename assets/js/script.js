87e9323c4c90055fbd90c0972d2608a879801659
Senay Gebrat <senaygebrat@gmail.com>
2022-10-11 21:36:34 -0700

    added comments to html, CSS, and js files

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,17 @@ var searchHistory = document.getElementById("search-history");
 var forecast = document.getElementById("forecast");
 var cityInput = document.getElementById("city");
 var search = document.getElementById("submit-search")
+var day1
+var day2
+var day3
+var day4
+var day5
 
 forecastEl = document.getElementById("forecast")
 forecastEl.style.display = "none"
 
+
+  //function to output the current temperature, humidity, and wind speeds from user input
 function currentDay(city){
     var currentDayURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey + "&units=imperial";
     console.log(currentDayURL)
@@ -22,47 +29,32 @@ function currentDay(city){
         var cityCap = city.toUpperCase()
         todayEl.textContent = `${cityCap}(${today})`
         document.getElementById("date").append(todayEl)
-    
 
         document.getElementById("current-city").textContent = "";
         var tempP = document.createElement("p");
         tempP.textContent = "Temp: " + data.main.temp + " °F";
         document.getElementById("current-city").append(tempP)
-        console.log(tempP);
-        // getForecast(data.coord.lat, data.coord.lon);
 
         var iconPic = data.weather[0].icon
         document.getElementById("icon").textContent = "";
         var icon = document.createElement("img");
         icon.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic + "@2x.png") 
         document.getElementById("icon").append(icon)
-        // getForecast(data.coord.lat, data.coord.lon);
-        console.log(icon);
 
         document.getElementById("humidity").textContent = "";
         var humidity = document.createElement("p");
         humidity.textContent = "Humidity: " + data.main.humidity + "%";
         document.getElementById("humidity").append(humidity)
-        // getForecast(data.coord.lat, data.coord.lon);
-        console.log(humidity);
 
         document.getElementById("wind").textContent = "";
         var wind = document.createElement("p");
         wind.textContent = "Wind: " + data.wind.speed + " MPH";
         document.getElementById("wind").append(wind)
-        // getForecast(data.coord.lat, data.coord.lon);
-        console.log(wind);
 
-    var day1 = moment().add(1,"day").format("MMMM Do, YYYY");
-    var day2 = moment().add(2,"day").format("MMMM Do, YYYY");
-    var day3 = moment().add(3,"day").format("MMMM Do, YYYY");
-    var day4 = moment().add(4,"day").format("MMMM Do, YYYY");
-    var day5 = moment().add(5,"day").format("MMMM Do, YYYY");
 
     forecastEl.style.display = "block"
 
-    console.log(day1)
-
+  //declaring variables for the 5 day forecast
     var element1 = document.getElementById("date-day-1")
     element1.textContent = day1
 
@@ -81,7 +73,7 @@ function currentDay(city){
 })
 }
 
-
+//function to get the forecast
 function getForecast(city){
     console.log(city);
     var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
@@ -100,6 +92,8 @@ function getForecast(city){
     })
     
 }
+
+//using the moments method to output the next 5 days
 function displayForecast(data){
     var day1 = moment().add(1,"day").format("MMMM Do, YYYY");
     var day2 = moment().add(2,"day").format("MMMM Do, YYYY");
@@ -107,26 +101,13 @@ function displayForecast(data){
     var day4 = moment().add(4,"day").format("MMMM Do, YYYY");
     var day5 = moment().add(5,"day").format("MMMM Do, YYYY");
 
-    forecastEl.style.display = "block"
+    forecastEl.style.display = "flex";
+    forecastEl.style.flex_basis = "auto";
 
-    console.log(day1)
-
-    // var iconPic = data.weather[0].icon
-    // document.getElementById("icon").textContent = "";
-    // var icon = document.createElement("img");
-    // icon.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic + "@2x.png") 
-    // document.getElementById("icon").append(icon)
-    // // getForecast(data.coord.lat, data.coord.lon);
-    // console.log(icon);
-
-
-
-
-
+    //day 1 of 5 day forecast, declaring variables locally for the weather icon, temperature, humidity, and wind
+    //iterates through the array for the information
     var element1 = document.getElementById("date-day-1")
     element1.textContent = day1
-
-
     var iconPic1 = data.list[3].weather[0].icon
     document.getElementById("icon1").textContent = "";
     var icon1 = document.createElement("img");
@@ -139,7 +120,10 @@ function displayForecast(data){
     var wind1 = document.getElementById("wind1")
     wind1.textContent = "Wind: " + data.list[3].wind.speed + " MPH";
 
-    //add icon2
+      //day 2 of 5 day forecast, declaring variables locally for the weather icon, temperature, humidity, and wind
+    //iterates through the array for the information
+    var element2 = document.getElementById("date-day-2")
+    element2.textContent = day2
     var iconPic2 = data.list[11].weather[0].icon
     document.getElementById("icon2").textContent = "";
     var icon2 = document.createElement("img");
@@ -152,12 +136,11 @@ function displayForecast(data){
     var wind2 = document.getElementById("wind2")
     wind2.textContent = "Wind: " + data.list[11].wind.speed + " MPH";
 
+    
+      //day 3 of 5 day forecast, declaring variables locally for the weather icon, temperature, humidity, and wind
+    //iterates through the array for the information
     var element3 = document.getElementById("date-day-3")
     element3.textContent = day3
-
-
-    //add icon3
-
     var iconPic3 = data.list[19].weather[0].icon
     document.getElementById("icon3").textContent = "";
     var icon3 = document.createElement("img");
@@ -170,10 +153,11 @@ function displayForecast(data){
     var wind3 = document.getElementById("wind3")
     wind3.textContent = "Wind: " + data.list[19].wind.speed + " MPH";
 
+   
+    //day 1 of 5 day forecast, declaring variables locally for the weather icon, temperature, humidity, and wind
+    //iterates through the array for the information
     var element4 = document.getElementById("date-day-4")
     element4.textContent = day4
-
-    //add icon4
     var iconPic4 = data.list[27].weather[0].icon
     document.getElementById("icon4").textContent = "";
     var icon4 = document.createElement("img");
@@ -188,15 +172,15 @@ function displayForecast(data){
 
 
 
-
+    //day 5 of 5 day forecast, declaring variables locally for the weather icon, temperature, humidity, and wind
+    //iterates through the array for the information
     var element5 = document.getElementById("date-day-5")
     element5.textContent = day5
-
-
     var iconPic5 = data.list[35].weather[0].icon
     document.getElementById("icon5").textContent = "";
     var icon5 = document.createElement("img");
-    icon5.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic5 + "@2x.png") 
+    icon5.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic5 + "@2x.png")
+    document.getElementById("icon5").append(icon5) 
     var temp5 =document.getElementById("temp5")
     temp5.textContent = "Temp: " + data.list[35].main.temp + " °F";
     var hum5 =document.getElementById("hum5")
@@ -206,7 +190,7 @@ function displayForecast(data){
 
 }
 
-
+//function to store user input values locally
 function saveCities(searchCity)
 {
     var cities = JSON.parse(localStorage.getItem("city")) || [];
@@ -215,26 +199,10 @@ function saveCities(searchCity)
 }
 
 search.addEventListener("click", function(event){
-    event.preventDefault();
-
- 
-    
+    event.preventDefault(); 
     var searchCity = cityInput.value;
     currentDay(searchCity);
     saveCities(searchCity);
     getForecast(searchCity);
     cityInput.value = "";
-
-   
-
-
-
-    //FUNCTIONALITY
-
-// once Search is clicked ...
-// condition statement in case searchCity is blank (what do i want it to do) -- refresh / display "Invalid search"
-
-
-// ELSE -- USING searchCity var 
-
 })
