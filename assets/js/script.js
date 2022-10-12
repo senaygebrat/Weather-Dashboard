ba881a05afa5a85476b1390daede057594195365
Senay Gebrat <senaygebrat@gmail.com>
2022-10-11 17:46:16 -0700

    updated function for forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,38 +47,42 @@ function currentDay(city){
         document.getElementById("date").append(todayEl)
     
 
-
-        console.log(JSON.stringify(data));
         document.getElementById("current-city").textContent = "";
         var tempP = document.createElement("p");
-        tempP.textContent = "Temp: " + data.main.temp;
+        tempP.textContent = "Temp: " + data.main.temp + " degrees °F";
         document.getElementById("current-city").append(tempP)
         console.log(tempP);
-        getForecast(data.coord.lat, data.coord.lon);
-
-        // document.getElementById("icon").textContent = "";
-        // var icon = document.createElement("p");
-        // icon.textContent = data.weather[3].value;
-        // document.getElementById("icon").append(icon)
         // getForecast(data.coord.lat, data.coord.lon);
-        // console.log(icon);
+
+        var iconPic = data.weather[0].icon
+        document.getElementById("icon").textContent = "";
+        var icon = document.createElement("img");
+        icon.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic + "@2x.png") 
+        document.getElementById("icon").append(icon)
+        // getForecast(data.coord.lat, data.coord.lon);
+        console.log(icon);
 
         document.getElementById("humidity").textContent = "";
         var humidity = document.createElement("p");
-        humidity.textContent = "Humidity: " + data.main.humidity;
+        humidity.textContent = "Humidity: " + data.main.humidity + " %";
         document.getElementById("humidity").append(humidity)
-        getForecast(data.coord.lat, data.coord.lon);
+        // getForecast(data.coord.lat, data.coord.lon);
         console.log(humidity);
 
         document.getElementById("wind").textContent = "";
         var wind = document.createElement("p");
-        wind.textContent = "Wind: " + data.wind.speed;
+        wind.textContent = "Wind: " + data.wind.speed + " MPH";
         document.getElementById("wind").append(wind)
-        getForecast(data.coord.lat, data.coord.lon);
+        // getForecast(data.coord.lat, data.coord.lon);
         console.log(wind);
 
+        // forecastEl.style.display = "block"
 
-         var day1 = moment().add(1,"day").format("MMMM Do, YYYY");
+
+ 
+
+
+    var day1 = moment().add(1,"day").format("MMMM Do, YYYY");
     var day2 = moment().add(2,"day").format("MMMM Do, YYYY");
     var day3 = moment().add(3,"day").format("MMMM Do, YYYY");
     var day4 = moment().add(4,"day").format("MMMM Do, YYYY");
@@ -103,36 +107,99 @@ function currentDay(city){
     var element5 = document.getElementById("date-day-5")
     element5.textContent = day5
 
+})
+}
 
 
-
-    // document.getElementById("card-title").textContent = ""
-    // var dayOneEl = 
-    
-
-    })
-  }
-// currentDay();
-function getForecast(lat, lon){
-    var forecastURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`; //need to change to line 31
-    console.log(forecastURL);
+function getForecast(city){
+    console.log(city);
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
     fetch(forecastURL)
     .then(function(response){
-        return response.json();
+        if(response.ok){
+            return response.json()
+            .then(function(data){
+                console.log(data)
+                // displayForecast(data)
+            })
+        }
+        else {
+            alert("Error: " + response.statusText)
+        }
     })
-    .then(function(data){
-        console.log(JSON.stringify(data));
-    });
     
 }
 function displayForecast(data){
+    var day1 = moment().add(1,"day").format("MMMM Do, YYYY");
+    var day2 = moment().add(2,"day").format("MMMM Do, YYYY");
+    var day3 = moment().add(3,"day").format("MMMM Do, YYYY");
+    var day4 = moment().add(4,"day").format("MMMM Do, YYYY");
+    var day5 = moment().add(5,"day").format("MMMM Do, YYYY");
 
-}
-function displayCurrentDay(data){
-    //create var for data. temperature humidity etc
-    //call created element var's, and append var that hold 
-    //data to those elements
-    //append those elements with data to div
+    forecastEl.style.display = "block"
+
+    console.log(day1)
+
+    var element1 = document.getElementById("date-day-1")
+    element1.textContent = day1
+    var icon1 = document.getElementsById("icon1")
+    var iconPic1 = //whatever data input from console (data.list[3].weather.icon)
+    icon1.setAttribute("src", "http://openweathermap.org/img/wn/" + iconPic1 + "@2x.png") 
+
+    var temp1 =document.getElementsById("temp1")
+    temp1.textContent = "Temp: " + data.list[3].main.temp;
+    var hum1 =document.getElementsById("hum1")
+    hum1.textContent = "Humidity " + data.list[3].main.humidity;
+    var wind1 = document.getElementsById("wind1")
+    wind1.textContent = "Wind" + data.list[3].wind.speed;
+
+    //add icon2
+    var element2 = document.getElementById("date-day-2")
+    element2.textContent = day2
+
+    var temp2 =document.getElementsById("temp2")
+    temp2.textContent = "Temp: " + data.list[3].main.temp;
+    var hum2 =document.getElementsById("hum2")
+    hum2.textContent = "Humidity " + data.list[3].main.humidity;
+    var wind2 = document.getElementsById("wind2")
+    wind2.textContent = "Wind" + data.list[3].wind.speed;
+
+    var element3 = document.getElementById("date-day-3")
+    element3.textContent = day3
+
+
+    //add icon3
+    var temp3 =document.getElementsById("temp3")
+    temp3.textContent = "Temp: " + data.list[3].main.temp;
+    var hum3 =document.getElementsById("hum3")
+    hum1.textContent = "Humidity " + data.list[3].main.humidity;
+    var wind3 = document.getElementsById("wind3")
+    wind1.textContent = "Wind" + data.list[3].wind.speed;
+
+    var element4 = document.getElementById("date-day-4")
+    element4.textContent = day4
+
+    //add icon4
+    var temp4 =document.getElementsById("temp4")
+    temp4.textContent = "Temp: " + data.list[3].main.temp;
+    var hum4 =document.getElementsById("hum4")
+    hum1.textContent = "Humidity " + data.list[3].main.humidity;
+    var wind4 = document.getElementsById("wind4")
+    wind1.textContent = "Wind" + data.list[3].wind.speed;
+
+
+
+
+    var element5 = document.getElementById("date-day-5")
+    element5.textContent = day5
+
+    //add icon5
+    var temp5 =document.getElementsById("temp5")
+    temp5.textContent = "Temp: " + data.list[3].main.temp;
+    var hum5 =document.getElementsById("hum5")
+    hum1.textContent = "Humidity " + data.list[3].main.humidity;
+    var wind5 = document.getElementsById("wind5")
+    wind1.textContent = "Wind" + data.list[3].wind.speed;
 
 }
 
@@ -146,25 +213,16 @@ function saveCities(searchCity)
 search.addEventListener("click", function(event){
     event.preventDefault();
 
-    //VARIABLES
-
-    //declaring variable for city input
+ 
+    
     var searchCity = cityInput.value;
-    cityInput.value = "";
     currentDay(searchCity);
     saveCities(searchCity);
-    //getForecast(searchCity);
-    //variable for api url
-    // var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey;
-    //variable for 
-    // var fiveForecastUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=" + apiKey;
-    //variables by day for forecast
-    // var day1 = moment().add(1,"day").format("MMM Do YY");
-    // var day2 = moment().add(2,"day").format("MMM Do YY");
-    // var day3 = moment().add(3,"day").format("MMM Do YY");
-    // var day4 = moment().add(4,"day").format("MMM Do YY");
-    // var day5 = moment().add(5,"day").format("MMM Do YY");
-    // var today = moment().format("MMM Do YY");
+    getForecast(searchCity);
+    cityInput.value = "";
+
+   
+
 
 
     //FUNCTIONALITY
